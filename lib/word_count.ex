8853618d9f174f49d5bdf7d358be83250e8c76e6
Cory Schmitt <cory@schmitty.me>
2015-07-06 12:57:40 -0700

    Count words in a sentence

--- lib/word_count.ex
+++ lib/word_count.ex
@@ -1,2 +1,15 @@
 defmodule WordCount do
+  def count(words) when is_list(words) do
+    Enum.reduce(words, %{}, &update_count/2)
+  end
+
+  def count(sentence) do
+    sentence
+    |> String.split
+    |> count
+  end
+
+  def update_count(word, acc) do
+    Map.update acc, String.to_atom(word), 1, &(&1 + 1)
+  end
 end
--- test/word_count_test.exs
+++ test/word_count_test.exs
@@ -1,7 +1,15 @@
 defmodule WordCountTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "single word sentence" do
+    assert WordCount.count("hey") == %{hey: 1}
+  end
+
+  test "two different words" do
+    assert WordCount.count("hey man") == %{hey: 1, man: 1}
+  end
+
+  test "same word twice" do
+    assert WordCount.count("hey man hey") == %{hey: 2, man: 1}
   end
 end
